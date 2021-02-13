32cd798858f26fd2c863de5612bdc871aa7750f1
Anto Safta <safta.antoneta@gmail.com>
2021-02-13 14:43:14 +0200

    work

--- external.js
+++ external.js
@@ -1,2 +1,4 @@
-//alert("Salut");
+alert("Salut");
 confirm("Esti de acord cu prelucrarea datelor cu caracter personal ? ");
+prompt("Care este greutatea ta ?");
+prompt("Care este inaltimea ta ?");
